d0bc843b5ab5eddb8d0e69e6d17ae943870ebba7
Laryd <hillarymuboka@gmail.com>
2022-03-22 16:39:20 +0300

    Deployment

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
-const User = require("../models/User");
+const User = require("../models/user");
 const Contact = require("../models/Contact");
 const { request } = require("express");
 
